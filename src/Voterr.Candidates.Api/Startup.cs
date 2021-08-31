4f742f3982c8ac01874c700214a8c663eeee0eed
Anders Refsdal Olsen <andersro93@ros-nett.com>
2021-08-31 18:08:27 +0200

    Some more cleanup

--- src/Voterr.Candidates.Api/Startup.cs
+++ src/Voterr.Candidates.Api/Startup.cs
@@ -10,30 +10,27 @@ namespace Voterr.Candidates.Api
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+        
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
 
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services
                 .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-                .AddMicrosoftIdentityWebApi(Configuration.GetSection("AzureAdB2C"));
+                .AddMicrosoftIdentityWebApi(Configuration.GetSection("AzureAd"));
 
             services.AddControllers();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
             }
 
             app.UseHttpsRedirection();
